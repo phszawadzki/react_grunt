9e45cabd046788b5ff6c2e5c4b94d9af5cb29178
phszawadzki <phszawadzki@gmail.com>
2022-06-09 19:11:28 +0200

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './about.css'
+// import ME from '../../assets/me.png'
+
+
+const about = () => {
+  return (
+      <div>
+          about
+      </div>
+    // <section id="about">
+    //     jestę inżynierę
+    // </section>
+  )
+}
+
+export default about
+
+
